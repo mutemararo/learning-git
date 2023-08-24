af8bb91a638bcf2cbe272758dee6e0f06539d167
mutemararo <rmutemararo@gmail.com>
2023-08-24 21:31:43 +0200

    CSS Transition Practice

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello git");
+
+const checkbox = document.querySelector("input");
+
+const button = document.querySelector("button");
+
+const square = document.querySelector(".square");
+
+checkbox.addEventListener("change", ({ target }) => {
+    if(checkbox.checked){
+        square.classList.add("has-transition");
+    }else{
+        square.classList.remove("has-transition");
+    }
+
+});
